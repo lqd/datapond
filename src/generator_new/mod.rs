25fc3e85b6debb77ee3e4b011aa844d51f30b373
Vytautas Astrauskas <vastrauskas@gmail.com>
2020-01-14 16:32:13 +0100

    Fix error reporting to produce valid token streams.

--- src/generator_new/mod.rs
+++ src/generator_new/mod.rs
@@ -1,5 +1,5 @@
 use proc_macro2::TokenStream;
-use quote::ToTokens;
+use quote::{ToTokens, quote};
 
 mod ast;
 mod encode;
@@ -8,9 +8,18 @@ mod to_tokens;
 pub fn generate_datafrog(input: TokenStream) -> TokenStream {
     let parsed_program = match syn::parse2(input) {
         Ok(program) => program,
-        Err(err) => return TokenStream::from(err.to_compile_error()),
+        Err(err) => {
+            let tokens = TokenStream::from(err.to_compile_error());
+            return quote! { {#tokens }};
+        },
     };
-    let typechecked_program = crate::typechecker::typecheck(parsed_program).unwrap();
+    let typechecked_program = match crate::typechecker::typecheck(parsed_program) {
+        Ok(program) => program,
+        Err(err) => {
+            let tokens = TokenStream::from(err.to_syn_error().to_compile_error());
+            return quote! { {#tokens }};
+        },
+    };
     let encoded_program = encode::encode(typechecked_program);
     encoded_program.to_token_stream()
 }
